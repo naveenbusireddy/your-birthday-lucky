4e7dd3844e820188bda530d60207bce609153445
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-22 13:00:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
+
