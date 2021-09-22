2aa63e68a4fe6c17cb528a4db22bf4460d6cf198
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-22 17:45:47 +0530

    update: App.js

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 
+const luckyValue = luckyNumber.value;
+// let sum = 0;
+function checkBirthdayLuckyNumber() {
+    const dob = (dateOfBirth.value).replaceAll("-", "");    
+    const sum = calculateSum(dob);
+}
 
-checkButton.addEventListener("click", function checkLuckyNumber() {
+function calculateSum(dob) 
+{
+    let sum = 0;
+    for(let  i=0; i<dob.length; i++) 
+    {
+        sum = sum+Number(dob.charAt[i]);
+    }    
+    return sum;
+}
     
-    console.log(dateOfBirth.value, luckyNumber.value);
-});
+function checkLuckyNumber(sum, luckyValue) 
+{
+    if(sum % luckyValue === 0 )
+        {
+            console.log("your Birthday is lucky");
+        }
+        else 
+        {
+            console.log("Your Birthday is not lucky")
+        }
+}
+
+// console.log(sum, luckyNumber.value);
+checkLuckyNumber();
+checkButton.addEventListener("click", checkBirthdayLuckyNumber);
