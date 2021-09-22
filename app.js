3f1ada1e59fc724830e4b377301ec8ae7761426f
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-22 16:00:44 +0530

    Update: getting input date

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 
+
+checkButton.addEventListener("click", function checkLuckyNumber() {
+    
+    console.log(dateOfBirth.value, luckyNumber.value);
+});
