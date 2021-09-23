910407e4c4eec6ac0304e020bed7a442cd44f525
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-23 12:47:31 +0530

    fix: Birthday Lucky Number

--- app.js
+++ app.js
@@ -3,26 +3,10 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
 const outputBox = document.querySelector("#output-box");
 
-const luckyValue = luckyNumber.value;
 // let sum = 0;
-function checkBirthdayLuckyNumber() {
-    const dob = (dateOfBirth.value).replaceAll("-", "");    
-    const sum = calculateSum(dob);
-}
-
-function calculateSum(dob) 
-{
-    let sum = 0;
-    for(let  i=0; i<dob.length; i++) 
-    {
-        sum = sum+Number(dob.charAt[i]);
-    }    
-    return sum;
-}
-    
 function checkLuckyNumber(sum, luckyValue) 
 {
-    if(sum % luckyValue === 0 )
+    if(sum%luckyValue===0)
         {
             outputBox.innerText="your Birthday is lucky";
         }
@@ -32,6 +16,21 @@ function checkLuckyNumber(sum, luckyValue)
         }
 }
 
+function checkBirthdayLuckyNumber() {
+    const dob = (dateOfBirth.value).replaceAll("-", "");    
+    const luckyValue = luckyNumber.value;
+    const sum = calculateSum(dob);
+    checkLuckyNumber(sum, luckyValue);
+}
+
+function calculateSum(dob) 
+{   let sum = 0;
+    for(let  index=0; index<dob.length; index++) {
+        sum = sum+Number(dob.charAt(index));
+    }    
+    return sum;
+}
+    
 // console.log(sum, luckyNumber.value);
-checkLuckyNumber();
+// checkLuckyNumber();
 checkButton.addEventListener("click", checkBirthdayLuckyNumber);
