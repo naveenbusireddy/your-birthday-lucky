9eed9ae8961c1504d9c26615555feb57b22e159e
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-23 13:10:58 +0530

    Update: Style

--- app.js
+++ app.js
@@ -4,39 +4,31 @@ const checkButton = document.querySelector("#check-button");
 const outputBox = document.querySelector("#output-box");
 
 // let sum = 0;
-function checkLuckyNumber(sum, luckyValue) 
-{
-    if(sum&&luckyValue) {
-        if(sum%luckyValue===0)
-        {
-            outputBox.innerText="your Birthday is lucky";
-        }
-        else 
-        {
-            outputBox.innerText="Your Birthday is not lucky";
-        }
-    }
-    else {
-        outputBox.innerText="Both Birthday and Lucky Number are required!"
-    }
-   
-}
-
 function checkBirthdayLuckyNumber() {
-    const dob = (dateOfBirth.value).replaceAll("-", "");    
+    const dob = (dateOfBirth.value).replaceAll("-", "");
     const luckyValue = luckyNumber.value;
     const sum = calculateSum(dob);
     checkLuckyNumber(sum, luckyValue);
 }
 
-function calculateSum(dob) 
-{   let sum = 0;
-    for(let  index=0; index<dob.length; index++) {
-        sum = sum+Number(dob.charAt(index));
-    }    
+function calculateSum(dob) {
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
     return sum;
 }
-    
-// console.log(sum, luckyNumber.value);
-// checkLuckyNumber();
+
+function checkLuckyNumber(sum, luckyValue) {
+    if (sum && luckyValue) {
+        if (sum % luckyValue === 0) {
+            outputBox.innerText = "your Birthday is lucky 😊😊";
+        } else {
+            outputBox.innerText = "Your Birthday is not lucky 😫😫";
+        }
+    } else {
+        outputBox.innerText = "Both Birthday and Lucky Number are required! 😡"
+    }
+}
+
 checkButton.addEventListener("click", checkBirthdayLuckyNumber);
